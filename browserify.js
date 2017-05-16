5d2ac89e0e939eb5c0fbe2ae74f97fae57116116
Bijou Trouvaille <sometimes@bijou4u.irc>
2017-05-16 12:12:35 -0500

    trimmed useless opts

--- browserify.js
+++ browserify.js
@@ -1,18 +1,9 @@
-var fs = require("fs");
 var browserify = require("browserify");
 var rollupify = require("rollupify")
 
 var rootPath = process.cwd()
-var distPath = rootPath + '/build'
 
-var opt = { 
-  extensions: ['.js'],
-  paths: ['./node_modules', './src'],
-  debug: true
-}
-
-
-var b = browserify(rootPath+"/src/index.js", opt)
+var b = browserify(rootPath+"/src/index.js")
   .transform(rollupify)
   .bundle()
   .pipe(process.stdout);
